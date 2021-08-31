be989e40f15d83797fa95acd6b20f7037f1e8377
YukiNagao2020 <yuki.n0301@gmail.com>
2021-08-31 04:39:48 +0000

    users index layout updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         
+  validates :name, presence: true    
+  validates :introduction, presence: true, length: { maximum: 200 }
 
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
